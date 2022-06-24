6c0730c0f7faf6f253c1472f2390b6e3ecdd58d4
rodriguezviktor92 <rodriguezviktor@gmail.com>
2022-06-24 15:38:01 -0500

    fix(src/js/service/fetchUsers): fix local storage check if users exist

--- src/js/service/fetchUsers.js
+++ src/js/service/fetchUsers.js
@@ -1,18 +1,22 @@
-const page1 = fetch('https://reqres.in/api/users?page=1').then((response) =>
-  response.json()
-);
-const page2 = fetch('https://reqres.in/api/users?page=2').then((response) =>
-  response.json()
-);
-
 function getUsers() {
-  const users = localStorage.getItem('userList');
+  const users = localStorage.getItem('usersList');
   if (users) {
-    console.log(users);
+    return JSON.parse(users);
   } else {
-    return Promise.allSettled([page1, page2]).then((values) => {
+    return Promise.allSettled([
+      fetch('https://reqres.in/api/users?page=1').then((response) =>
+        response.json()
+      ),
+      fetch('https://reqres.in/api/users?page=2').then((response) =>
+        response.json()
+      ),
+    ]).then((values) => {
       const [{ value: page1 }, { value: page2 }] = values;
       if (page1.data.length && page2.data.length) {
+        localStorage.setItem(
+          'usersList',
+          JSON.stringify([...page1.data, ...page2.data])
+        );
         return [...page1.data, ...page2.data];
       }
     });
